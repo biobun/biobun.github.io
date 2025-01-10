35a0570579f3d66166c341f2dd2c186ab7946fb7
Bio Abidzar <biobun@gmail.com>
2025-01-10 11:24:00 +0700

    fix: change basePath

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/biobun.github.io/next.svg"
+          src="./next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +32,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/biobun.github.io/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/biobun.github.io/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/biobun.github.io/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -88,7 +88,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/biobun.github.io/globe.svg"
+            src="./globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
